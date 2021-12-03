74213759994b6a1c76e4b02d03fd40a0c1ee30c8
hitk1 <luispaulo.degini@gmail.com>
2021-12-03 14:05:56 -0300

    style: Estilizacao dos header

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -2,13 +2,32 @@ import React from 'react'
 import { View, Text } from 'react-native'
 
 import {
-    Container
+    Container,
+    Header,
+    UserWrapper,
+    UserInfo,
+    Photo,
+    User,
+    UserGreeting,
+    UserName,
 } from './styles'
 
 export const Dashboard = () => {
     return (
         <Container>
-            <Text>Dashboard</Text>
+            <Header>
+                <UserWrapper>
+                    <UserInfo>
+                        <Photo
+                            source={{ uri: 'https://avatars.githubusercontent.com/u/35121576?v=4' }}
+                        />
+                        <User>
+                            <UserGreeting>Olá</UserGreeting>
+                            <UserName>Luis Paulo</UserName>
+                        </User>
+                    </UserInfo>
+                </UserWrapper>
+            </Header>
         </Container>
     )
 }
